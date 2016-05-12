96f98e936ffc66701071034835db228f08adf926
Ethan Hur <ethan0311@gmail.com>
2016-05-12 13:50:46 +0000

    adding unit test

--- test/controllers/labels_controller_test.rb
+++ test/controllers/labels_controller_test.rb
@@ -4,4 +4,52 @@ class LabelsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test '#create_label' do
+    post :create, {'name' => 'label' }
+    json = JSON.parse(response.body)
+    assert json['name'] == 'label'
+    assert json['item'] == 0
+  end
+
+  test '#read_label' do
+    post :create, {'name' => 'label_read1'}
+    post :create, {'name' => 'label_read2'}
+
+    get :read
+    json = JSON.parse(response.body)
+    flag1 = false
+    flag2 = false
+
+    json.each do |json_entity|
+      if json_entity['name'] == 'label_read1'
+        flag1 = true
+      end
+      if json_entity['name'] == 'label_read2'
+        flag2 = true
+      end
+    end
+    assert flag1 == true
+    assert flag2 == true
+  end
+
+  test '#tag_label' do
+
+  end
+
+  test '#untag_label' do
+
+  end
+
+  test '#delete_label' do
+    post :create, {'name' => 'label' }
+    json = JSON.parse(response.body)
+    label_id = json['eid']
+
+    post :delete, { 'id' => label_id }
+    json = JSON.parse(response.body)
+    assert label_id == json['eid']
+  end
+
+  
 end
--- test/controllers/notes_controller_test.rb
+++ test/controllers/notes_controller_test.rb
@@ -4,4 +4,52 @@ class NotesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test '#create_note_success' do
+    post :create, {'subject' => 'subject', 'content' => 'content'}
+    json = JSON.parse(response.body)
+    assert json['subject'] == 'subject'
+  end
+
+  test '#read_note' do
+    post :create, {'subject' => 'subject_read1', 'content' => 'content1'}
+    post :create, {'subject' => 'subject_read2', 'content' => 'content2'}
+
+    get :read
+    json = JSON.parse(response.body)
+    flag1 = false
+    flag2 = false
+    json.each do |json_entity|
+      if json_entity['subject'] == 'subject_read1'
+        flag1 = true
+      end
+      if json_entity['subject'] == 'subject_read2'
+        flag2 = true
+      end
+    end
+    assert flag1 == true
+    assert flag2 == true
+  end
+
+  test '#update_note' do
+    post :create, {'subject' => 'subject3', 'content' => 'content3'}
+    json = JSON.parse(response.body)
+    note_id = json['eid']
+
+    post :update, {'subject' => 'new_subject', 'content' => 'new_content', 'id' => note_id}
+    json = JSON.parse(response.body)
+    assert note_id == json['eid']
+    assert json['subject'] == 'new_subject'
+  end
+
+  test '#delete_note' do
+    post :create, {'subject' => 'subject3', 'content' => 'content3'}
+    json = JSON.parse(response.body)
+    note_id = json['eid']
+
+    post :delete, {'id' => note_id }
+    json = JSON.parse(response.body)
+    assert note_id == json['eid']
+  end
+
 end
